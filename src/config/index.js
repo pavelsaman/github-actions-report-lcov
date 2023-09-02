eb6bff0549cca53c5f1b337248867ad76ac5f29e
Pavel Saman <samanpavel@gmail.com>
2023-09-02 22:23:02 +0200

    remove unused config values

--- src/config/index.js
+++ src/config/index.js
@@ -5,8 +5,7 @@ export const config = {
   common_lcov_args: ['--rc', 'lcov_branch_coverage=1'],
   action_msg_prefix: 'code coverage gh action:',
   failureEmoji: ':no_entry:',
-  detailsHeaderSize: 3,
-  collapseDetailsIfLines: 3,
+  collapseDetailsIfLines: 10,
   allowedGitHubEvents: ['pull_request', 'pull_request_target'],
   lcovTempDirectoryName: 'lcov-tmp-dir',
   prCommentTableHeader: [
